665d58bde893b471ed9a63fade2c1e0071bfebc7
Marika jane <66046690+marika-jane@users.noreply.github.com>
2020-09-24 11:04:11 +1200

    Update migration columns and seed data

--- server/db/migrations/20200923215024_users.js
+++ server/db/migrations/20200923215024_users.js
@@ -1,13 +1,15 @@
 exports.up = (knex) => {
   return knex.schema.createTable('users', (table) => {
     table.increments('id')
-    table.int('artwork_id') // for users own artwork
+    table.integer('artwork_id') // for users own artwork
     table.string('username')
     table.binary('hash')
     table.string('email')
     table.string('name')
     table.string('profile_picture')
     table.string('about_info')
+    table.boolean('is_artist')
+    table.boolean('is_charity')
     // table.integer('user_id').references('id').inTable('users');
   })
 }
--- server/db/seeds/1-artworks.js
+++ server/db/seeds/1-artworks.js
@@ -4,9 +4,9 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('artworks').insert([
-        { id: 1, cause_id: 'rowValue1', artwork_name: 'First Artwork', artwork_description: 'This artwork is about something', artwork_image: 'https://artforce.org/wp-content/uploads/2016/03/The-Birth-of-Venus.jpg', is_available: true },
-        { id: 2, cause_id: 'rowValue1', artwork_name: 'Second Artwork', artwork_description: 'This is an artwork inspired by the artist\'s passion for coding', artwork_image: 'https://media.timeout.com/images/103166743/image.jpg', is_available: true },
-        { id: 3, cause_id: 'rowValue1', artwork_name: 'Third Artwork', artwork_description: 'This is an artwork inspired by the artist\'s mum', artwork_image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtdZYKldcbOa8ryIDvt-hTQCPM8FklZYfZaw&usqp=CAU', is_available: true }
+        { id: 900, cause_id: 1, artist_id: 4, artwork_name: 'First Artwork', artwork_description: 'This artwork is about something', artwork_image: 'https://artforce.org/wp-content/uploads/2016/03/The-Birth-of-Venus.jpg', is_available: true },
+        { id: 901, cause_id: 1, artist_id: 3, artwork_name: 'Second Artwork', artwork_description: 'This is an artwork inspired by the artist\'s passion for coding', artwork_image: 'https://media.timeout.com/images/103166743/image.jpg', is_available: true },
+        { id: 902, cause_id: 2, artist_id: 1, artwork_name: 'Third Artwork', artwork_description: 'This is an artwork inspired by the artist\'s mum', artwork_image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtdZYKldcbOa8ryIDvt-hTQCPM8FklZYfZaw&usqp=CAU', is_available: true }
       ])
     })
 }
