f0090e4f0b28992fd002691b8e4af37db2c6b8d4
Lewis <lewisnpakoti@gmail.com>
2020-09-24 10:53:41 +1200

    created donart routes

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,22 @@
 const env = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[env]
 const connection = require('knex')(config)
+
+module.exports = {
+  getArtworks,
+  getUserArtwork
+}
+
+function getArtworks (db = connection) {
+  return db('gardens'.select()
+    .catch(err => {
+      console.error(err)
+      throw err
+    }))
+}
+
+function getUserArtwork (id, db = cconnection) {
+    return db ('artworks')
+        .where('artworks.id', id)
+        .
+}
