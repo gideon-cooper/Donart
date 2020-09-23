cd3fb1e55843631dde0b559acf51d1efd060baa6
betty-bucket <coulson.ev@gmail.com>
2020-09-24 11:43:35 +1200

    changed header background colour

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="has-background-primary">
+    <div className="has-background-light">
       <h1 className="is-size-1">React development has begun!</h1>
     </div>
   )
