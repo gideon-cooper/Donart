e012109d368c98f94333533d1bf91665c0954119
betty-bucket <coulson.ev@gmail.com>
2020-09-24 10:38:31 +1200

    added integer length in migration table

--- server/db/migrations/20200923215230_artworks.js
+++ server/db/migrations/20200923215230_artworks.js
@@ -1,14 +1,13 @@
-
 exports.up = (knex) => {
-    return knex.schema.createTable('artworks', (table) => {
-        table.increments('id')
-        table.int('cause_id')   // for artwork's charity (user id)
-        table.string('artwork_name')
-        table.string('artwork_description')
-        table.string('artwork_image')
-        })
-    }
-        
-exports.down = function(knex) {
-    return knex.schema.dropTable('artworks');
-} 
+  return knex.schema.createTable('artworks', (table) => {
+    table.increments('id')
+    table.int('cause_id', 4) // for artwork's charity (user id)
+    table.string('artwork_name')
+    table.string('artwork_description')
+    table.string('artwork_image')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('artworks')
+}
