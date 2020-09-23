35575e50918607dc62fe7a8572473230f5b7ac8a
gideon-cooper <Gideon.Cooper@hotmail.com>
2020-09-24 00:13:20 +1200

    added few starter files and installed packages

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
+import React from "react"
+import "../main.scss"
 const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+  return <h1>React development has begun!</h1>
 }
 
 export default App
