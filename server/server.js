3443df605a7bfd06b8a8a7e2786084d4d99ce92e
Gideon Cooper <Gideon.Cooper@hotmail.com>
2020-09-24 15:02:00 +1200

    merged with dev

--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
-// server.use('/api/v1', authRoutes)
-// server.use('/api/v1/donart', donartRoutes)
+server.use('/api/v1', authRoutes)
+server.use('/api/v1/donart', donartRoutes)
 module.exports = server
