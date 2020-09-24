99e7382f2e79fe3eca5fbab35c6ae61474a3dc01
Marika jane <66046690+marika-jane@users.noreply.github.com>
2020-09-24 16:35:34 +1200

    Add getArtworks function yay!

--- server/db/db.js
+++ server/db/db.js
@@ -10,8 +10,18 @@ function getArtworks (db = connection) {
   return db('artworks')
     .join('users as seller', 'seller.id', 'artworks.artist_id')
     .join('users as cause', 'cause.id', 'artworks.cause_id')
-    .select()
+    .select('artworks.id as id', 'artworks.name as artworkName', 'price', 'image', 'seller.id as sellerId', 'seller.name as sellerName', 'cause.id as causeId', 'cause.name as causeName')
+    .then(result => {
+      return result.map(artwork => {
+        return {
+          id: artwork.id,
+          image: artwork.image,
+          price: artwork.price,
+          sellerId: artwork.sellerId,
+          sellerName: artwork.sellerName,
+          causeId: artwork.causeId,
+          causeName: artwork.causeName
+        }
+      })
+    })
 }
-
-console.log(getArtworks())
-
