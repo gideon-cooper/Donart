97d51e9d98c3739f148aa5872fa71147f4c42e77
Marika Fiolitakis <marika.jf@gmail.com>
2020-09-24 22:58:31 +1200

    Add artwork name to DB function

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,8 @@ const config = require('./knexfile')[env]
 const connection = require('knex')(config)
 
 module.exports = {
-  getArtworks
+  getArtworks,
+  addNewArtwork
 }
 
 function getArtworks (db = connection) {
@@ -15,6 +16,7 @@ function getArtworks (db = connection) {
       return result.map(artwork => {
         return {
           id: artwork.id,
+          name: artwork.artworkName,
           image: artwork.image,
           price: artwork.price,
           artistId: artwork.artistId,
@@ -23,5 +25,21 @@ function getArtworks (db = connection) {
           causeName: artwork.causeName
         }
       })
+    })
+}
+
+function addNewArtwork (formData, db = connection) {
+    return db('artworks')
+    .join('users as artist', 'artist.id', 'artworks.artist_id')
+    .join('users as cause', 'cause.id', 'artworks.cause_id')
+    .insert({
+        image: formData.image,
+        name: formData.name,
+        description: formData.description,
+        price: formData.price,
+        artist_id: formData.artistId,
+        // artistName: formData.artistName,
+        // causeName: formData.causeName,
+        cause_id: formData.causeId
     })
 }
