24b9b4ae3baa9c0aef484e4d02a51eed0be0c628
Lewis <lewisnpakoti@gmail.com>
2020-09-25 12:17:17 +1200

    update

--- server/routes/donart.js
+++ server/routes/donart.js
@@ -1,10 +1,10 @@
-const express = require("express")
+const express = require('express')
 
-const db = require("../db/db")
+const db = require('../db/db')
 
 const router = express.Router()
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   db.getArtworks()
     .then((artworks) => {
       return res.json({ artworks })
@@ -14,16 +14,17 @@ router.get("/", (req, res) => {
     })
 })
 
-// router.get('/:id', (req, res) => {
-//   const id = Number(req.params.id)
-//   db.getUserArtwork(id)
-//     .then((singleArt) => {
-//       return res.json(singleArt)
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ error: err.message })
-//     })
-// })
+// GET artwork by id
+router.get('/:artwork_id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getArtworkById(id)
+    .then((singleArt) => {
+      return res.json(singleArt)
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message })
+    })
+})
 
 // POST /api/v1/donart/new-artwork
 router.post('/new-artwork', (req, res) => {
@@ -32,12 +33,24 @@ router.post('/new-artwork', (req, res) => {
   db.addNewArtwork(newArtwork)
     .then(result =>
       res.status(200).json(result))
-  //   .then(newId => {      // is an array of 1 number
-  //     db.getPostCommentsById(newId[0])
-  //     .then(result => res.json(camelcase(result[0])))
-  // })
+    //   .then(newId => {      // is an array of 1 number
+    //     db.getPostCommentsById(newId[0])
+    //     .then(result => res.json(camelcase(result[0])))
+    // })
     .catch(err => {
       res.status(500).send(err.message)
+    })
+})
+
+// GET user by id
+router.get('/:user_id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getUserById(id)
+    .then((singleUser) => {
+      return res.json(singleUser)
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message })
     })
 })
 
