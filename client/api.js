f470398651abf41e884c14d1142ee60cbb054e11
Marika Fiolitakis <marika.jf@gmail.com>
2020-09-25 17:39:24 +1200

    write getUsers API

--- client/api.js
+++ client/api.js
@@ -1,15 +1,23 @@
-import request from "superagent"
+import request from 'superagent'
 
-const rootUrl = "api/v1/donart"
-export function getArt() {
+const rootUrl = '/api/v1/donart'
+
+export function getArt () {
   return request.get(rootUrl).then((res) => {
-   
     return res.body
   })
 }
 
-export function getOneArt(artId) {
-  return request.get(rootUrl + "/art/" + artId).then((res) => {
+export function getOneArt (artId) {
+  return request.get(rootUrl + '/art/' + artId).then((res) => {
     return res.body
   })
+}
+
+export function getUsers () {
+  return request.get(`${rootUrl}/users`)
+    .then(res => {
+      console.log('USERS', res.body.users)
+      return res.body.users
+    })
 }
