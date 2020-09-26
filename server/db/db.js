e33fba5a8678b0071f4f6106ccb7a481677d0489
betty-bucket <coulson.ev@gmail.com>
2020-09-26 16:48:24 +1200

    add getArtistsbyID function and get user by id function

--- server/db/db.js
+++ server/db/db.js
@@ -10,7 +10,8 @@ module.exports = {
   addNewArtwork,
   artIsSold,
   editProfile,
-  getAllUsers
+  getAllUsers,
+  getArtistsbyID
 }
 
 function getArtworks(db = connection) {
@@ -94,6 +95,30 @@ function artIsSold (id, db = connection) {
 function getAllUsers (db = connection) {
   return db('users')
     .select()
+}
+
+function getArtistsbyID (id, db = connection) {
+  return db('users')
+  .join('artworks','artworks.artist_id','users.id')
+  .select('users.id as id', 'users.name as artistName', 'about', 'profile_picture as profilePicture', 'artworks.id as artworkID', 'email', 'artworks.name as artworkName', 'artworks.image as artImage', 'artworks.price as price')
+  .where('users.id', id)
+  .then(result => {
+    return {
+      id: result[0].id,
+      artistName: result[0].artistName,
+      about: result[0].about,
+      profilePicture: result[0].profilePicture,
+      email: result[0].email,
+      artworks: !result[0].artworkID ? [] : result.map(art => {
+        return {
+          id: art.artworkID,
+          name: art.artworkName,
+          image: art.artImage,
+          price: art.price
+        }
+      })
+    }
+  })
 }
 
 function editProfile(id, user, db = connection) {
--- server/db/seeds/1-artworks.js
+++ server/db/seeds/1-artworks.js
@@ -8,7 +8,7 @@ exports.seed = function (knex) {
         { id: 901, cause_id: 1, artist_id: 3, name: 'Second Artwork', price: 30, description: 'This is an artwork inspired by the artist\'s passion for coding', image: 'https://media.timeout.com/images/103166743/image.jpg', is_available: true },
         { id: 902, cause_id: 2, artist_id: 1, name: 'Third Artwork', price: 20, description: 'This is an artwork inspired by the artist\'s mum', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRtdZYKldcbOa8ryIDvt-hTQCPM8FklZYfZaw&usqp=CAU', is_available: true },
         { id: 903, cause_id: 3, artist_id: 5, name: 'Evelyn the acrobat', price: 15, description: 'This is an artwork inspired by Evelyn', image: '/images/Hayley_1.jpg', is_available: true },
-        { id: 904, cause_id: 3, artist_id: 5, name: 'Owl and me', price: 15, description: 'This is an artwork inspired by stuff', image: '/images/Hayley_2.jpg', is_available: true },
+        { id: 904, cause_id: 3, artist_id: 2, name: 'Owl and me', price: 15, description: 'This is an artwork inspired by stuff', image: '/images/Hayley_2.jpg', is_available: true },
         { id: 905, cause_id: 3, artist_id: 5, name: 'Happy Hayley', price: 15, description: 'This is an artwork inspired by stuff', image: '/images/Hayley_3.jpg', is_available: true },
         { id: 906, cause_id: 3, artist_id: 5, name: 'Hangry Cat', price: 15, description: 'This is an artwork inspired by pumpkin', image: '/images/Hayley_4.jpg', is_available: true },
         { id: 907, cause_id: 3, artist_id: 5, name: 'Fire in the Heart', price: 15, description: 'This is an artwork inspired by shit going down', image: '/images/Hayley_5.jpg', is_available: true },
