c86a2f738eb4635399b1af3d2265bc8dfbc18564
betty-bucket <coulson.ev@gmail.com>
2020-09-26 18:04:21 +1200

    add link to artworks page to individual art page

--- client/components/ArtworkItem.jsx
+++ client/components/ArtworkItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function ArtworkItem ({ artwork }) {
   const styles = {
@@ -7,13 +8,17 @@ export default function ArtworkItem ({ artwork }) {
     backgroundPosition: 'top center',
     height: '20em'
   }
+   const artworkID = String(artwork.id)
   return (
     <div className="card-flex-item card" style={{ margin: '20px' }}>
+      <Link to={`/ArtworkDetails/${artworkID}`}>
       <h4 className="has-text-centered mt-3">{artwork.name}</h4>
       <div className="mt-4 mb-6 mx-6" style={styles}></div>
       <p>
         {artwork.cause_id}
       </p>
+      </Link>
     </div>
+    
   )
 }
