b5a02e46a6dbd7c9d86d4fface6080ed113b157a
Marika Fiolitakis <marika.jf@gmail.com>
2020-09-26 21:00:32 +1200

    Saving images to DB now - probably a bit broken still

--- client/components/AddArtwork.jsx
+++ client/components/AddArtwork.jsx
@@ -19,8 +19,8 @@ export default function AddArtwork () {
   const [loading, setLoading] = useState(false)
 
   const uploadImage = async e => {
-      const files = e.target.files
-      console.log(e.target.files)
+    const files = e.target.files
+    console.log(e.target.files)
     const data = new FormData()
     data.append('file', files[0])
     data.append('upload_preset', 'donart')
@@ -33,7 +33,7 @@ export default function AddArtwork () {
       }
     )
     const file = await res.json()
-    console.log('file res.json: ', file.url)
+    console.log('file res.json: ', file.secure_url)
 
     setImage(file.secure_url)
     setLoading(false)
@@ -103,55 +103,55 @@ export default function AddArtwork () {
   return (
     <div className="form">
       <h1>List Your Artwork</h1>
-      {/* <form onSubmit={handleSubmit}> */}
+      <form onSubmit={handleSubmit}>
 
-      <h5>Artwork Name</h5>
-      <input className="" type="text"
-        placeholder="Artwork Name"
-        name="name"
-        value={name} onChange={event => setName(event.target.value)}/>
+        <h5>Artwork Name</h5>
+        <input className="" type="text"
+          placeholder="Artwork Name"
+          name="name"
+          value={name} onChange={event => setName(event.target.value)}/>
 
-      <h5>Price</h5>
-      <input className="" type="number"
-        placeholder="Artwork Price"
-        name="price"
-        value={price} onChange={event => setPrice(event.target.value)}/>
+        <h5>Price</h5>
+        <input className="" type="number"
+          placeholder="Artwork Price"
+          name="price"
+          value={price} onChange={event => setPrice(event.target.value)}/>
 
-      <h5>Description</h5>
-      <textarea className="" type="text"
-        placeholder="Artwork description"
-        name="description"
-        value={description} onChange={event => setDescription(event.target.value)}/>
+        <h5>Description</h5>
+        <textarea className="" type="text"
+          placeholder="Artwork description"
+          name="description"
+          value={description} onChange={event => setDescription(event.target.value)}/>
 
-      <h5>Upload Image</h5>
-      <input className="" type="file"
-        placeholder="Browse"
-        // name="image"
-        name="file"
-        // value={fileInput} onChange={handleFileInputChange}/>
-        onChange={uploadImage} />
-      {loading ? (
-        <h3>Loading...</h3>
-      ) : (
-        <img src={image} alt="" style={{ width: '300px' }}/>
-      )}
-      <button
+        <h5>Upload Image</h5>
+        <input className="" type="file"
+          placeholder="Browse"
+          // name="image"
+          name="file"
+          // value={fileInput} onChange={handleFileInputChange}/>
+          onChange={uploadImage} />
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <img src={image} alt="" style={{ width: '300px' }}/>
+        )}
+        <button
         //   onClick={handleUploadClick}
-        className="" type="submit">Upload</button>
+          className="" type="submit">Upload</button>
 
-      <h5>Select your Cause</h5>
-      <select name="cause" onChange={event => setCause(event.target.value)}>
-        <option value="">--Select your cause from the list--</option>
-        {causes.map(cause => {
-          return <option key={cause.id}
-            name="singleCause"
-            value={cause.id}
-          >{cause.name}</option>
-        })}
-      </select>
+        <h5>Select your Cause</h5>
+        <select name="cause" onChange={event => setCause(event.target.value)}>
+          <option value="">--Select your cause from the list--</option>
+          {causes.map(cause => {
+            return <option key={cause.id}
+              name="singleCause"
+              value={cause.id}
+            >{cause.name}</option>
+          })}
+        </select>
 
-      <button className="button my-4 is-primary">Create Listing</button>
-      {/* </form> */}
+        <button className="button my-4 is-primary">Create Listing</button>
+      </form>
 
       {/* {previewSource && (
         <img src={previewSource} style={{ height: '300px' }} alt=""/>
