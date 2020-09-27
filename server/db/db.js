a00dfe0b98d6fa3bbebcbe5e2a4bd9fc66981b75
betty-bucket <coulson.ev@gmail.com>
2020-09-28 10:56:08 +1300

    capitalise function on getAllUsers db function

--- server/db/db.js
+++ server/db/db.js
@@ -116,6 +116,19 @@ function artIsSold (id, db = connection) {
 function getAllUsers (db = connection) {
   return db('users')
     .select()
+    .then((result) => {
+      return result.map((user) => {
+        return {
+          id: user.id,
+          username: user.username,
+          name: capitalizeFirstLetter(user.name),
+          hash: user.hash,
+          email: user.email,
+          profile_picture: user.profile_picture,
+          about: user.about
+        }
+      })
+    })
 }
 
 function getArtistsbyID (id, db = connection) {
