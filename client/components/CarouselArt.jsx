d97e1c187eab5297f628c317546fe8481ffc316a
Lewis <lewisnpakoti@gmail.com>
2020-09-28 17:48:04 +1300

    organized main.scss file in style folder with labels/comments

--- client/components/CarouselArt.jsx
+++ client/components/CarouselArt.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useContext, useEffect } from "react"
-import { Link, useHistory } from "react-router-dom"
+import React, { useState, useContext, useEffect } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
-import AddToCart from "./AddToCart"
+import AddToCart from './AddToCart'
 
 export default function CarouselArt(props) {
   const artworkId = String(props.art.id)
@@ -9,14 +9,15 @@ export default function CarouselArt(props) {
   return (
     <div className='carouselArt'>
       <Link to={`/ArtworkDetails/${artworkId}`}>
-        <p>Art name: {props.art.artworkName}</p>
-        <p>Artist: {props.art.artistName}</p>
-        <p>Cause: {props.art.causeName}</p>
         <img
           src={props.art.image}
-          style={{ width: "200px", height: "200px" }}
+          style={{ width: '200px', height: '200px' }}
           alt=''
         />
+        <p className="ACProperty">Artist Name:</p><p>{props.art.artworkName}</p>
+        <p>Artist: {props.art.artistName}</p>
+        <p>Cause: {props.art.causeName}</p>
+
       </Link>
       {}
 
