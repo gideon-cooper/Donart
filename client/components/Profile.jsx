fc42e737f040500a91552f21f3b8dc16a241b8fa
Marika Fiolitakis <marika.jf@gmail.com>
2020-09-29 13:37:40 +1300

    Show user's own listings on personal profile page

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -2,54 +2,67 @@ import React, { useState, useEffect, useContext } from 'react'
 
 import { UserContext, updateUserContext } from './UserContext'
 import { Redirect, Link } from 'react-router-dom'
-import Footer from './Footer'
 import { getArtist, getUser } from '../api'
 
-export default function Profile() {
+import ProfileArtListings from './ProfileArtListings'
+import Footer from './Footer'
+
+export default function Profile () {
   const [user, setUser] = useContext(UserContext)
   // Kept these comments Just In Case something breaks... delete later
   // const userId = String(user.id)
   // const [profileData, setProfileData] = useState({})
+  const [artist, setArtist] = useState({})
 
-  // console.log('User from context:', user)
+  // console.log('User from context:', user.id)
   // console.log('User ID from context:', userId)
 
-  // useEffect(() => {
-  //   console.log('User ID (in component): ', userId)
-  //   if (userId !== null) {
-  //     getUser(userId)
-  //       .then(result => {
-  //         console.log('user profile result: ', result)
-  //         setProfileData(result)
-  //         return result
-  //       })
-  //       .catch((err) => console.log("error:", err.message))
-  //   }
-  // }, [user])
+  useEffect(() => {
+    console.log('User ID (in component): ', user.id)
+    if (user.id !== null) {
+      getArtist(String(user.id))
+        .then(result => {
+          console.log('user profile result: ', result)
+          setArtist(result)
+          return result
+        })
+        .catch((err) => console.log('error:', err.message))
+    }
+  }, [user])
 
   return (
-    <div className="profile">
-      <div className="topProfile">
-        <div className="leftProfile">
-          {user.name ? <h1>{user.name}</h1> : <h1>Please update your profile details</h1>}
-          
-          <img src={user.image} alt="" />
-          <p>Your bio: {user.about}</p>
-          <p>Current email address: {user.email}</p>
-          {user.isCharity ? <p>You are listed as a charity</p> : <p>You are not listed as a charity</p>}
-          {user.isArtist ? <p>You are listed as an artist</p> : <p>You are not listed as an artist</p>}
-          <div className="profileButtons">
-            <Link to={`/editProfile/${user.id}`}>
-              <button>Edit Profile</button>
-            </Link>
-            <Link to={'/add-artwork'}>
-              <button>Add Artwork</button>
-            </Link>
+    <>
+      <div className="profile">
+        <div className="topProfile">
+          <div className="leftProfile">
+            {user.name ? <h1>{user.name}</h1> : <h1>Please update your profile details</h1>}
+
+            <img src={user.image} alt="" />
+            <p>Your bio: {user.about}</p>
+            <p>Current email address: {user.email}</p>
+            {user.isCharity ? <p>You are listed as a charity</p> : <p>You are not listed as a charity</p>}
+            {user.isArtist ? <p>You are listed as an artist</p> : <p>You are not listed as an artist</p>}
+            <div className="profileButtons">
+              <Link to={`/editProfile/${user.id}`}>
+                <button>Edit Profile</button>
+              </Link>
+              <Link to={'/add-artwork'}>
+                <button>Add Artwork</button>
+              </Link>
+            </div>
+          </div>
+          <div className="rightProfile">
+            {(artist.artworks === undefined || artist.artworks.length === 0)
+              ? <p>You have not listed any artworks</p>
+              : artist.artworks.map(art => (<>
+                <p>Your Artwork Listings</p>
+                <ProfileArtListings key={art.id} artwork={art} bio={false} />
+              </>))
+            }
           </div>
         </div>
-        <div className="rightProfile"></div>
       </div>
       <Footer />
-    </div>
+    </>
   )
 }
