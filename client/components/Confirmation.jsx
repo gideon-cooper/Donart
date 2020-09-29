0718b06933116af58b17518fc841ac6c29d9cb65
Lewis <lewisnpakoti@gmail.com>
2020-09-29 16:32:14 +1300

    styled footer and checkout component with icons and text styling

--- client/components/Confirmation.jsx
+++ client/components/Confirmation.jsx
@@ -5,7 +5,17 @@ export default function Confirmation() {
   return (
     <>
       <div className="confirmation">
-        <h1>Thank you for purchasing, your items will soon be with you.</h1>
+        <img
+          src={'./images/icons/green-tick.png'}
+          style={{ width: '2.5rem', height: '2.5rem' }}
+          alt="delivery icon"
+        />
+        <h1 className="confirmationMessage">Thank you for purchasing, your items will soon be with you.</h1>
+        <img
+          src={'./images/icons/delivery-icon-grey.png'}
+          style={{ width: '10rem', height: '10rem' }}
+          alt="delivery icon"
+        />
       </div>
       <Footer />
     </>
