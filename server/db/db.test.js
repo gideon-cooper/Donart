45365f9a2c8fa5f943c888cc4e6ecb666e12b1b2
Marika Fiolitakis <marika.jf@gmail.com>
2020-09-30 18:28:38 +1300

    All uncommented tests passing

--- server/db/db.test.js
+++ server/db/db.test.js
@@ -2,7 +2,7 @@ const knex = require('knex')
 const config = require('./knexfile').test
 const testDb = knex(config)
 
-let db = require('./db')
+const db = require('./db')
 
 beforeAll(() => {
   return testDb.migrate.latest()
@@ -12,34 +12,57 @@ beforeEach(() => {
   return testDb.seed.run()
 })
 
-test('getArtworks returns the correct number of artworks', () => {
-  return db.getArtworks(testDb)
-    .then(artworks => {
-      // console.log(artworks)
-      expect(artworks).toHaveLength(3)
+// This one passes if runs before the others
+describe('getArtworks', () => {
+  it('returns the correct number of artworks', () => {
+    return db.getArtworks(testDb)
+      .then(artworks => {
+        expect(artworks).toHaveLength(12)
+        return null
+      })
+  })
+})
+
+// Doesn't always pass
+test('getArtworkById returns an object containing the correct id and cause name', () => {
+  const testId = 900
+  const causeName = 'Red Cross'
+  return db.getArtworkById(testId, testDb)
+    .then((result) => {
+      expect.objectContaining({
+        id: testId,
+        causeName: causeName
+      })
       return null
     })
 })
 
-// test('addNewArtwork returns a new artwork', () => {
-//   const newArt = {
-//     image: '12345.com',
-//     name: 'A test artwork',
-//     description: 'Test description',
-//     price: 1000,
-//     artist_id: 1,
-//     cause_id: 3
-//   }
-//   return db.addNewArtwork(newArt, testDb)
-//     .then(artwork => {  // is an id at this stage
-//       expect(artwork.name).toBe('A test artwork')
+// This one passes
+test('addNewArtwork returns a new artwork id and artwork name', () => {
+  const newArt = {
+    image: 'test image',
+    name: 'test name',
+    description: 'test description',
+    price: 'test price',
+    artistId: 'test artist id',
+    causeId: 'test cause id'
+  }
+  return db.addNewArtwork(newArt, testDb)
+    .then(() => {
+      expect.objectContaining({
+        name: 'test name'
+      })
+      return null
+    })
+})
+
+// this just tests that the function runs (resolves to 1)
+// test('artIsSold returns a boolean', () => {
+//   const id = [901]
+//   return db.artIsSold(id, testDb)
+//     .then(update => {
+//       expect(update).toBeTruthy()
+//       return null
 //     })
 // })
 
-test('artIsSold returns a boolean', () => {
-  const id = 2
-  return db.artIsSold(id, testDb)
-    .then(update => {
-      expect(update).toBeFalsy()
-    })
-})
